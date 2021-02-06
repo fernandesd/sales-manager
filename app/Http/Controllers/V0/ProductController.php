905ed9205281db349d54d6fd5128e10103c920ff
Diego Fernandes <diegofernandes.dev@gmail.com>
2021-02-06 10:14:54 -0300

    initial commit

---
+++ app/Http/Controllers/V0/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\V0;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = Product::all();
+        $products->each(function($product){
+            return $product->setImageUrl();
+        });
+
+        return response()
+                ->json(['status' => 200, 'message'=>'Consulta realizada com sucesso!', 'data'=>$products]);
+    }
+}
